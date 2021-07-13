7e6ec6264f37952c6087d51687d4bacf8507e855
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-07-13 12:00:09 -0400

    storing data using userdefualts

--- core_date_final/core_date_final/TodoListVewController.swift
+++ core_date_final/core_date_final/TodoListVewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["1", "2", "3"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK - Tableview Datasource Methods
@@ -54,6 +59,9 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the add item button on our UIAlert
             self.itemArray.append(textField.text!)
+            
+            self.defaults.setValue(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         

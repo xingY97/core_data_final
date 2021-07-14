51d969f6242ecbf95c9ef5ae813225abbc1f5bbd
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-07-14 13:05:07 -0400

    fixed searchBar location to be on top of prototype cell

--- core_date_final/core_date_final/Controllers/TodoListVewController.swift
+++ core_date_final/core_date_final/Controllers/TodoListVewController.swift
@@ -133,6 +133,10 @@ extension TodoListViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
         }
     }
 }

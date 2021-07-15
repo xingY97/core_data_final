86a95bd1fab666bb75dd7c76a0a25d8071aaf8a9
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-07-15 00:57:01 -0400

    added tableView delete method

--- core_date_final/core_date_final/Controllers/CategoryViewController.swift
+++ core_date_final/core_date_final/Controllers/CategoryViewController.swift
@@ -49,6 +49,15 @@ class CategoryViewController: UITableViewController {
         }
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            categories.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
     func saveCategories() {
         
         do {
--- core_date_final/core_date_final/Controllers/TodoListVewController.swift
+++ core_date_final/core_date_final/Controllers/TodoListVewController.swift
@@ -67,6 +67,15 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            itemArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
     //MARK: - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
